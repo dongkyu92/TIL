bd65c81c4b9b78cecb2c60eea76505c5331cc7f8
dongkyu92 <dongkyu92@naver.com>
2020-09-24 10:34:15 +0900

    collection 프레임워크 복습

---
+++ Java/Grammar/collection/Product2.java
@@ -0,0 +1,41 @@
+package collection;
+
+public class Product2 {
+	private String p1;
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	private String p2;
+	public String getP1() {
+		return p1;
+	}
+	public void setP1(String p1) {
+		this.p1 = p1;
+	}
+	public String getP2() {
+		return p2;
+	}
+	public void setP2(String p2) {
+		this.p2 = p2;
+	}
+	public String getP3() {
+		return p3;
+	}
+	public void setP3(String p3) {
+		this.p3 = p3;
+	}
+	private String p3;
+
+}
