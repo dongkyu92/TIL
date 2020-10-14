b3c167a5a44607c0708b73ced6805f94f9a4b9f5
dongkyu92 <dongkyu92@naver.com>
2020-10-14 17:04:56 +0900

    코딩테스트_해시_위장

--- Python/Algorithm/(Hash)위장.py
+++ Python/Algorithm/(Hash)위장.py
@@ -14,8 +14,17 @@ def solution(clothes):
         answer *= (i + 1)
 
     return answer - 1
-
+# defaultdict를 썼으니 if else 비교는 필요 없다.
 
 # TestCase
 solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])
 #solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]])
+
+
+# 깔끔한 풀이
+# def solution(clothes):
+#     from collections import Counter
+#     from functools import reduce
+#     cnt = Counter([kind for name, kind in clothes])
+#     answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+#     return answer
