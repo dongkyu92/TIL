b9b520c2a2f3e12002bc1f6761fac427e847d5f5
dongkyu92 <dongkyu92@naver.com>
2020-10-14 17:03:59 +0900

    하노이의탑 재귀로 풀어야함

---
+++ Python/Algorithm/(SkillTest) Level3 하노이의 탑.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = [[]]
+    
+    top1 = [ i for i in range(1, n + 1) ]
+    top2 = []
+    top3 = []
+    top2.append(top1.pop(0))
+    
+    # for i for range(n):
+    #     if n % 2 == 0:
+             
+    #     else:
+
+    return answer
+
+
+print(solution(3))
