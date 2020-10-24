dd20db219c11e9c53758daf283d700d2ad56e177
dongkyu92 <dongkyu92@naver.com>
2020-10-24 16:29:25 +0900

    (10814) 나이순 정렬 수정

--- Python/BACKJOON/(10814)나이순 정렬.py
+++ Python/BACKJOON/(10814)나이순 정렬.py
@@ -1,15 +1,5 @@
-N = int(input())
-members = []
-for i in range(N):
-    age, name = input().split()
-    members.append([age, name])
-members.sort(key=lambda x: (x[0]), reverse=True)
 
-for member in members:
-    print(member[0]+ ' ' + member[1])
-
-
-    member_num = int(input())
+member_num = int(input())
 member_list = []
 
 for _ in range(member_num):
@@ -17,7 +7,6 @@ for _ in range(member_num):
     member_age = int(member_age)
     member_list.append((member_age, member_name))
 
-#나이 숫자 정렬 > 가입순 정렬
 member_list.sort(key = lambda member: (member[0]))
 
 for member in member_list:
