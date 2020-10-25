689ac44d0e682c90ff59f4f41483d19cc7059309
dongkyu92 <dongkyu92@naver.com>
2020-10-25 18:13:26 +0900

    주석 추가

--- Python/BACKJOON/(10814)나이순 정렬.py
+++ Python/BACKJOON/(10814)나이순 정렬.py
@@ -6,7 +6,8 @@ for _ in range(member_num):
     member_age, member_name = map(str, input().split())
     member_age = int(member_age)
     member_list.append((member_age, member_name))
-
+# _는 아무 값이나 상관 없다는 뜻.
+# 반복만 member_num 만큼 해준다.
 member_list.sort(key = lambda member: (member[0]))
 
 for member in member_list:
