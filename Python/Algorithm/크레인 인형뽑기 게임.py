de2dfe81827cb703a70fcf9281b2d6506a9e0f15
dongkyu92 <dongkyu92@naver.com>
2020-10-26 16:10:01 +0900

    (다이나믹 프로그래밍) 크레인 인형뽑기 게임- 실패

---
+++ Python/Algorithm/크레인 인형뽑기 게임.py
@@ -0,0 +1,34 @@
+def solution(board, moves):
+    answer = 0
+    newBoard = []
+    cart = []
+    
+    for i in range(0, len(board)):
+        boardList = []
+        for k in board:
+            boardList.append(k[i])
+        newBoard.append(boardList)
+        boardList = []
+    
+    for i in moves:
+        for index, j in enumerate(newBoard[i - 1]):
+            if j != 0:
+                cart.append(j)
+                newBoard[i - 1][index] = 0
+                break
+
+    flag = True
+    while flag == True:
+        for i, c in enumerate(cart):
+            if cart[i] == cart[i - 1]:
+                answer += 2
+                cart.remove(c)
+                cart.remove(cart[i - 1])
+                if i == len(cart) - 1:
+                  flag = False           
+
+    return answer
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+solution(board, moves)
