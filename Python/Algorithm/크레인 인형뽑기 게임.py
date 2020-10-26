b596e342d9cc5349366011987ee72a194de7f595
dongkyu92 <dongkyu92@naver.com>
2020-10-26 16:50:44 +0900

    크레인 인형뽑기 게임 - 2차시도 실패

--- Python/Algorithm/크레인 인형뽑기 게임.py
+++ Python/Algorithm/크레인 인형뽑기 게임.py
@@ -1,22 +1,44 @@
+# def solution(board, moves):
+#     answer = 0
+#     newBoard = []
+#     cart = []
+    
+#     for i in range(0, len(board)):
+#         boardList = []
+#         for k in board:
+#             boardList.append(k[i])
+#         newBoard.append(boardList)
+#         boardList = []
+    
+#     for i in moves:
+#         for index, j in enumerate(newBoard[i - 1]):
+#             if j != 0:
+#                 cart.append(j)
+#                 newBoard[i - 1][index] = 0
+#                 break
+
+#     flag = True
+#     while flag == True:
+#         for i, c in enumerate(cart):
+#             if cart[i] == cart[i - 1]:
+#                 answer += 2
+#                 cart.remove(c)
+#                 cart.remove(cart[i - 1])
+#                 if i == len(cart) - 1:
+#                   flag = False           
+
+#     return answer
+# ---------------------------------------------------------------------------------------------------------
 def solution(board, moves):
     answer = 0
-    newBoard = []
     cart = []
-    
-    for i in range(0, len(board)):
-        boardList = []
-        for k in board:
-            boardList.append(k[i])
-        newBoard.append(boardList)
-        boardList = []
-    
-    for i in moves:
-        for index, j in enumerate(newBoard[i - 1]):
-            if j != 0:
-                cart.append(j)
-                newBoard[i - 1][index] = 0
+    for move in moves:
+        for i, b in enumerate(board):
+            if b[move - 1] != 0:
+                cart.append(b[move - 1])
+                board[i][move - 1] = 0
                 break
-
+    
     flag = True
     while flag == True:
         for i, c in enumerate(cart):
@@ -28,6 +50,7 @@ def solution(board, moves):
                   flag = False           
 
     return answer
+
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
