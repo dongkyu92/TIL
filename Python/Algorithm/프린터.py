eaec4c5c61ac6325d69cd5fd93de3a45375eedcc
dongkyu92 <dongkyu92@naver.com>
2020-10-27 17:45:10 +0900

    (우선순위 큐) 프린터 런타임에러 3개

--- Python/Algorithm/프린터.py
+++ Python/Algorithm/프린터.py
@@ -1,18 +1,32 @@
+# import heapq
+# def solution(priorities, location):
+#     answer = 0
+#     h = []
+#     for priority in priorities:
+#         heapq.heappush(h, (100 - priority, priority))
+#     print(heapq.heappop(h))
+#     return answer
+
+# deque를 활용하면 더 빨라짐.
+# deque.rotate(n) 활용.
+# 양수면 오른쪽으로 회전, 음수면 왼쪽으로 회전.
+
+from collections import deque
 def solution(priorities, location):
     answer = 0
-    for i, priority in enumerate(priorities):
-        if Location == i:
-            for j in priorities[i:len(priorities - 1)]:
-                if j > priority:
-                    priorities.pop(0)
-                    answer += 1
+    d = deque([(v, i) for i, v in enumerate(priorities)])
+    # max값을 쉽게 찾기 위해 i, v의 위치를 바꾼다. (v, i) 소괄호 필수
+
+    while len(d):
+        temp = d.popleft()
+        if max(d)[0] > temp[0] and d:
+            d.append(temp)
+
         else:
-            priorities.pop(0)
             answer += 1
-
-        
+            if temp[1] == location:
+                break
     return answer
-
 
 solution([2, 1, 3, 2], 2)
 solution([1, 1, 9, 1, 1, 1], 0)
