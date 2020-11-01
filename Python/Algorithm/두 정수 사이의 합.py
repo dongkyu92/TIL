d496a651267f073a3433b83ecb4c548a20baaf01
dongkyu92 <dongkyu92@naver.com>
2020-11-01 17:34:22 +0900

    두 정수 사이의 합

---
+++ Python/Algorithm/두 정수 사이의 합.py
@@ -0,0 +1,8 @@
+def solution(a, b):
+    answer = 0
+    if a > b:
+        answer = sum([x for x in range(b, a + 1)])
+    else:
+        answer = sum([x for x in range(a, b + 1)])
+    return answer
+print(solution(3, 5))
