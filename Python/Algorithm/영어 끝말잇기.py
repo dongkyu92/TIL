e3be081b5452a5c16fba66e1b2a102c05f273514
dongkyu92 <dongkyu92@naver.com>
2020-11-04 11:27:51 +0900

    영어 끝말잇기 딕셔너리

--- Python/Algorithm/영어 끝말잇기.py
+++ Python/Algorithm/영어 끝말잇기.py
@@ -1,27 +1,22 @@
 from collections import defaultdict
 
 def solution(n, words):
-    answer = []
+    answer = [0, 0]
     dic = defaultdict(int)
-    
-    while True:
-        for i, word in enumerate(words):
-            print('for')
-            if word in dic:
-                print('for - if')
-                print(i % 3 + 1, i + 1)
-                answer.append(i % 3 + 1)
-                answer.append(i + 1)
-                break
-            else:
-                print('for - else')
-                dic[word] += 1
-             
+    for i, word in enumerate(words):
+        if word in dic or (words[i - 1][-1] if i != 0 else word[0]) != word[0] :
+            # print(words[i - 1][-1] if i!= 0 else word[0])
+            # print(word[0])
+            temp1 = (i + 1) % n if (i + 1) % n != 0 else n
+            temp2 = (i + 1) // n + 1 if (i + 1) % n != 0 else (i + 1) // n
+            # temp2 수정해야함. 어떨때는 n이고 어떨때는 n + 1
+            answer = [temp1, temp2]
+            return answer
         else:
-            print('else')
-            answer = [0, 0]
-            break
+            dic[word] += 1
+    else:
+        return answer
 
-    return answer
 
-print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
+# print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
