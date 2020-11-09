40c89c7711941f7e44ed54be9b22d0f6e77975f0
dongkyu92 <dongkyu92@naver.com>
2020-11-09 11:14:27 +0900

    짝지어 제거하기

--- Python/Algorithm/짝지어 제거하기.py
+++ Python/Algorithm/짝지어 제거하기.py
@@ -1,4 +1,23 @@
 def solution(s):
     answer = 0
-
+    s = list(s)
+    flag = True
+    while flag:
+        if len(s) == 0:
+            break
+        else:
+            for i in range(0, len(s)):
+                if i == len(s) - 1:
+                    return 0
+                else:
+                    if s[i] == s[i + 1]:
+                        s.pop(s.index(s[i]))
+                        s.pop(s.index(s[i]))
+                        break
+                    else:
+                        continue
+    answer = 1
     return answer
+
+# print(solution('cdcd'))
+print(solution('baabaa'))
