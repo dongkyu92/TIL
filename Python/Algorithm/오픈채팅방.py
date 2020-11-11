c151b8660bc58aff89a6ee3b7b386cd931eca683
dongkyu92 <dongkyu92@naver.com>
2020-11-11 17:46:21 +0900

    오픈채팅방 테스트케이스 6개 실패

--- Python/Algorithm/오픈채팅방.py
+++ Python/Algorithm/오픈채팅방.py
@@ -1,39 +1,43 @@
-from collections import defaultdict
 def solution(record):
     answer = []
-    id_dic = defaultdict(str)
-    message_dic = defaultdict(str)
+    uid_dic = {}
+    id_dic = {}
+    message_dic = {}
     for i in record:
         if i.split()[0] == 'Enter':
             if i.split()[1] in id_dic:
-                
                 id_dic[i.split()[1]] = i.split()[2]
+                uid_dic[i.split()[1]] = i.split()[1]
                 message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 들어왔습니다.'] 
-                answer.append( [id_dic[i.split()[1]], message_dic[i.split()[1]]])
+                answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
 
-                for index, m in enumerate(answer):
-                    if m[0] == id_dic[i.split()[1]]:
-                        answer[index][0] = i.split()[2]
-                        answer[index][1][0] = i.split()[2]
+                for index, ans in enumerate(answer):
+                    if ans[0] == uid_dic[i.split()[1]]: 
+                        answer[index][0] = uid_dic[i.split()[1]]
+                        answer[index][1][0] = message_dic[i.split()[1]][0]
+
             else:
                 id_dic[i.split()[1]] = i.split()[2]
+                uid_dic[i.split()[1]] = i.split()[1]
                 message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 들어왔습니다.'] 
-                answer.append( [id_dic[i.split()[1]], message_dic[i.split()[1]]])
+                answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
         
         elif i.split()[0] == 'Leave':
             message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 나갔습니다.']
-            answer.append( [id_dic[i.split()[1]], message_dic[i.split()[1]]])
+            answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
 
             
         elif i.split()[0] == 'Change':
-            for index, m in enumerate(answer):
-                if m[0] == id_dic[i.split()[1]]:
-                    answer[index][0] = i.split()[2]
+            for index, ans in enumerate(answer):
+                if ans[0] == uid_dic[i.split()[1]]:
+                    id_dic[i.split()[1]] = i.split()[2]
+                    uid_dic[i.split()[1]] = i.split()[1]
+                    answer[index][0] = uid_dic[i.split()[1]]
                     answer[index][1][0] = i.split()[2]
-
+    result = []
     for i in answer:
-        print(''.join(i[1]))       
-    return answer
+        result.append(''.join(i[1]))       
+    return result
 
 
 print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
