69f1ddc9511fcb34227ba8d7dc056b04da6dd67b
dongkyu92 <dongkyu92@naver.com>
2020-11-11 22:15:15 +0900

    오픈채팅방 코드 수정

--- Python/Algorithm/오픈채팅방.py
+++ Python/Algorithm/오픈채팅방.py
@@ -3,37 +3,43 @@ def solution(record):
     uid_dic = {}
     id_dic = {}
     message_dic = {}
-    for i in record:
-        if i.split()[0] == 'Enter':
-            if i.split()[1] in id_dic:
-                id_dic[i.split()[1]] = i.split()[2]
-                uid_dic[i.split()[1]] = i.split()[1]
-                message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 들어왔습니다.'] 
-                answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
-
+    for i, _ in enumerate(record):
+        record[i] = record[i].split()
+    
+    for i, _ in enumerate(record):
+        if record[i][0] == 'Enter':
+            if record[i][1] in id_dic:
+                id_dic[record[i][1]] = record[i][2]
+                uid_dic[record[i][1]] = record[i][1]
+                message_dic[record[i][1]] = [id_dic[record[i][1]], '님이 들어왔습니다.'] 
+                answer.append([uid_dic[record[i][1]], message_dic[record[i][1]]])
+                # id_dic이 필요?
+                # answer에 넣어서 for 문으로 찾지말고 딕셔너리에 리스트를 넣어서
+                # 최악이 N2이 아니라 선형이 되도록 변경
+                
                 for index, ans in enumerate(answer):
-                    if ans[0] == uid_dic[i.split()[1]]: 
-                        answer[index][0] = uid_dic[i.split()[1]]
-                        answer[index][1][0] = message_dic[i.split()[1]][0]
+                    if ans[0] == uid_dic[record[i][1]]: 
+                        answer[index][0] = uid_dic[record[i][1]]
+                        answer[index][1][0] = message_dic[record[i][1]][0]
 
             else:
-                id_dic[i.split()[1]] = i.split()[2]
-                uid_dic[i.split()[1]] = i.split()[1]
-                message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 들어왔습니다.'] 
-                answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
+                id_dic[record[i][1]] = record[i][2]
+                uid_dic[record[i][1]] = record[i][1]
+                message_dic[record[i][1]] = [id_dic[record[i][1]], '님이 들어왔습니다.'] 
+                answer.append( [uid_dic[record[i][1]], message_dic[record[i][1]]])
         
-        elif i.split()[0] == 'Leave':
-            message_dic[i.split()[1]] = [id_dic[i.split()[1]], '님이 나갔습니다.']
-            answer.append( [uid_dic[i.split()[1]], message_dic[i.split()[1]]])
+        elif record[i][0] == 'Leave':
+            message_dic[record[i][1]] = [id_dic[record[i][1]], '님이 나갔습니다.']
+            answer.append( [uid_dic[record[i][1]], message_dic[record[i][1]]])
 
             
-        elif i.split()[0] == 'Change':
+        elif record[i][0] == 'Change':
             for index, ans in enumerate(answer):
-                if ans[0] == uid_dic[i.split()[1]]:
-                    id_dic[i.split()[1]] = i.split()[2]
-                    uid_dic[i.split()[1]] = i.split()[1]
-                    answer[index][0] = uid_dic[i.split()[1]]
-                    answer[index][1][0] = i.split()[2]
+                if ans[0] == uid_dic[record[i][1]]:
+                    id_dic[record[i][1]] = record[i][2]
+                    uid_dic[record[i][1]] = record[i][1]
+                    answer[index][0] = uid_dic[record[i][1]]
+                    answer[index][1][0] = record[i][2]
     result = []
     for i in answer:
         result.append(''.join(i[1]))       
