228f23ae77d38979558582c22e56b9e2852d75fe
dongkyu92 <dongkyu92@naver.com>
2020-11-14 20:49:41 +0900

    모의고사 푸는중

---
+++ Python/Algorithm/모의고사.py
@@ -0,0 +1,14 @@
+def solution(answers):
+    answer = []
+    s1 = [1, 2, 3, 4, 5]
+    s2 = [2, 1, 2, 3, 2 ,4 ,2 ,5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4 ,5, 5]
+    temp = []
+    
+    for i in enumerate(answer):
+        if s1[i] = i % 5:
+            
+    return answer
+
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 3, 2, 4, 2]))
