c06344ffd8e23866ddb25868e61f9ad1effef4ec
dongkyu92 <dongkyu92@naver.com>
2020-11-17 17:41:54 +0900

    문자열 압축

--- Python/Algorithm/문자열 압축.py
+++ Python/Algorithm/문자열 압축.py
@@ -3,14 +3,29 @@
 from collections import deque
 def solution(s):
     answer = 0
-    LENGTH = len(s_dq)
-    s_dq = deque(s) # 문자열 s를 deque로 바꿈.
     
-    for i in range(LENGTH):
-        splited_s_dq = [ s[size:size + i] for size in range(0, LENGTH, i)]
+    # 문자열 s를 deque로 바꿈.
+    LENGTH = len(s)
+    
+    for i in range(1, LENGTH):
+        splited_s_dq = [ s[size:size + i] for size in range(0, LENGTH, i) ]
         # n개 단위로 잘라내는 작업.
         # range의 3번째 인자는 숫자 사이의 거리
         # splited_s_dq라는 리스트를 만들어서 리스트컨프리헨젼으로 n개 단위로 나눈 값으로 초기화
+        temp = []
+        splited_s_dq = deque(splited_s_dq)
+        for i, _ in range(1, len(splited_s_dq)):
+            if splited_s_dq[i] == splited_s_dq[i - 1]:
+                if splited_s_dq[i - 1].isdigit():
+                    splited_s_dq.popleft()
+                    splited_s_dq.appendleft()
+                else:
+                    splited_s_dq.popleft()
+                    splited_s_dq.appendleft(int(2))
+
+                
+                continue
+
     return answer
 
 print(solution("aabbaccc"))
