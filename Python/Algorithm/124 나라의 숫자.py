f0bf6bb21adcd8ac769ea954df66d29823df8214
dongkyu92 <dongkyu92@naver.com>
2020-11-20 13:15:40 +0900

    124 나라의 숫자

---
+++ Python/Algorithm/124 나라의 숫자.py
@@ -0,0 +1,19 @@
+def solution(n):
+    answer = ''
+    number = [1, 2, 4]
+    
+    share = n // 3
+    remainder = n % 3
+
+    flag = True
+    while flag:
+        if share > 3:
+        
+        else:
+        
+
+    return answer
+
+
+print(solution(10))
+print(solution(15))
