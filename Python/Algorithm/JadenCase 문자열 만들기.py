8f80fe43d93bf2f47aecb793b53c85e16691d629
dongkyu92 <dongkyu92@naver.com>
2020-11-23 22:59:07 +0900

    모르겟다

--- Python/Algorithm/JadenCase 문자열 만들기.py
+++ Python/Algorithm/JadenCase 문자열 만들기.py
@@ -1,8 +1,17 @@
 import string
 def solution(s):
-    s = [ (string.capwords(i.lower())) if i[0].isalpha() else i.lower() for i in s.split() ]
-        
+    s = [ string.capwords(i.lower()) for i in s.split() ]
+    # return ' '.join(s)
+    # answer = []
+    # s = s.split()
+    # for i in s:
+    #     if i[0].isdigit():
+    #         answer.append(i.lower())
+    #     else:
+    #         answer.append(string.capwords(i.lower()))
     return ' '.join(s)
 
 print(solution('fdsjgsj fllf kfkef ww'))
 print(solution('3people unFollowed me'))
+print(solution('3people unFollowed me'))
+print(solution('34eople u4Followed me4'))
