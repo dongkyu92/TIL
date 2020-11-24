1bd3f80e937838b7b5497bc1473782753538061c
dongkyu92 <dongkyu92@naver.com>
2020-11-24 13:58:01 +0900

    다리를 지나는 트럭

--- Python/Algorithm/다리를 지나는 트럭.py
+++ Python/Algorithm/다리를 지나는 트럭.py
@@ -1,32 +1,12 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    truck_in_bridge = []
-    truck_in_bridge = deque(truck_in_bridge)
-    truck_weight_in_bridge = 0
     time = 0
-    passd_truck = 0
+    q = [0] * bridge_length
 
-    while True:
+    while q:
         time += 1
-        if truck_weights:
-            if weight >= truck_weights[0] + truck_weight_in_bridge and bridge_length > len(truck_in_bridge):
-                # 다리의 허용 무게 >= 대기중인 트럭 무게 + 다리위에 있는 트럭 무게
-                # and 다리 길이 > 트럭(길이 1) 개수
-                truck_in_bridge.append([truck_weights[0], time]) # 트럭 개수 + 1
-                truck_weight_in_bridge += int(truck_weights.pop(0)) # + 트럭 무게
-                for truck in truck_in_bridge:
-                    truck[1] = time
-
-            else:
-                for truck in truck_in_bridge:
-                    truck[1] = time
-        else:
-            for truck in truck_in_bridge:
-                    truck[1] = time
-            break
-            
-    print(truck_in_bridge)
+        q.pop(0)
         
     return answer
 
