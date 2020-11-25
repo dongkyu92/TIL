9201b83e116640023c5a53074eeffb4b8b6aca85
dongkyu92 <dongkyu92@naver.com>
2020-11-25 11:44:50 +0900

    다리를 지나는 트럭

--- Python/Algorithm/다리를 지나는 트럭.py
+++ Python/Algorithm/다리를 지나는 트럭.py
@@ -1,14 +1,17 @@
-from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
     time = 0
     q = [0] * bridge_length
-
+    
     while q:
         time += 1
         q.pop(0)
-        
-    return answer
+        if truck_weights:
+            if sum(q) + truck_weights[0] <= weight:
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
+            
+    return time
 
 
 solution(2, 10, [7, 4, 5, 6])
