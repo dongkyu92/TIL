91353e550c83e1b09e1e05deefd2804e5a1fc0b5
dongkyu92 <dongkyu92@naver.com>
2020-11-27 10:46:30 +0900

    가장 긴 팰린드롬

--- Python/Algorithm/가장 긴 팰린드롬.py
+++ Python/Algorithm/가장 긴 팰린드롬.py
@@ -1,10 +1,14 @@
 def solution(s):
     answer = 0
     
-    for 
+    for i in range(len(s) - 1):
+        temp_before = s[:i + 1]
+        temp_after = s[:i + 2]
         
+        print(temp_before)
+        print(temp_after)
     return answer
 
 
 print(solution('abacde'))
-print(solution('abcdcba'))
+# print(solution('abcdcba'))
