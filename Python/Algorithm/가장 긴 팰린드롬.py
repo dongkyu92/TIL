e7aaa6a11fe77ac687c5d17d2987e89b31ed69f1
dongkyu92 <dongkyu92@naver.com>
2020-11-30 13:15:52 +0900

    가장 긴 팰린드롬

--- Python/Algorithm/가장 긴 팰린드롬.py
+++ Python/Algorithm/가장 긴 팰린드롬.py
@@ -1,3 +1,6 @@
+# 실행시간 생각보다 풀 생각을 먼저
+# slice, 재귀 활용해도 됨
+
 def solution(s):
     answer = 0
     
@@ -5,10 +8,20 @@ def solution(s):
         temp_before = s[:i + 1]
         temp_after = s[:i + 2]
         
-        print(temp_before)
-        print(temp_after)
+        m, l = 0, len(temp_before) - 1
+        if len(temp_before) % 2 != 0:
+            if len(temp_before) == 1:
+                answer += 1
+            else:
+                
+                if temp_before[m] == temp_before[l]:
+                    answer += 1
+                else:
+                    continue
+        else:
+            continue
     return answer
 
 
 print(solution('abacde'))
-# print(solution('abcdcba'))
+print(solution('abcdcba'))
